5f32a6bfc791e563719a674d564675bf4b69d44f
roxana.orha <roxanapascaru@gmail.com>
2024-02-05 09:02:13 +0200

    live test2

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styles from './ImageGalleryItem.module.css';
-import PropTypes from 'prop-types';
 
 const ImageGalleryItem = ({ webformatURL, id, onClick }) => {
   const handleClick = () => {
